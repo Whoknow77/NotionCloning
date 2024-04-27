a0d0ca86ebd0d47b7c4942b16458b0c5055eea86
Whoknow77 <gnsdh86166@gmail.com>
2024-04-27 15:08:11 +0900

    하위문서 padding 추가

--- src/utils/renderSubDocuments.js
+++ src/utils/renderSubDocuments.js
@@ -4,6 +4,7 @@ ${documents
 	.map(
 		(doc) => `<li data-id="${doc.id}" class="sub-document__item">
 <button>${doc.title}</button>
+${doc.documents && renderSubDocuments(doc.documents)}
 </li>`
 	)
 	.join("")}
